10209d02c2ce5d8bdb71f7c4fc68e7df162dc874
yunifuchioka <fuchioka@cs.ubc.ca>
2021-08-24 16:23:20 -0700

    test neural network latency within main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,15 @@
 #include "main.hpp"
+#include "network_policy.hpp"
 
 using namespace solo;
 
 static THREAD_FUNCTION_RETURN_TYPE control_loop(void* robot_if_void_ptr) {
   double dt = 0.001;
+
+  NetworkPolicy my_network_policy("iter9900"); // TODO: don't hardcode this
+
+  srand((unsigned int)time(0));  // initialize random seed
+  VectorObservation observation;
 
   MasterBoardInterface* robot_if =
       (static_cast<MasterBoardInterface*>(robot_if_void_ptr));
@@ -21,12 +27,25 @@ static THREAD_FUNCTION_RETURN_TYPE control_loop(void* robot_if_void_ptr) {
     rt_printf("\nTimeout while waiting for ack.\n");
   }
 
+  int counter = 0;
   while (!CTRL_C_DETECTED) {
     robot_if->ParseSensorData();
+
+    observation << Eigen::MatrixXd::Random(NETWORK_INPUT_DIM, 1);
+    VectorAction action = my_network_policy.get_action(observation);
+
+    if (counter % 1000 == 0) {
+      std::cout << "\x1B[2J\x1B[H";
+      std::cout << "counter: " << counter << std::endl;
+      std::cout << "timer: " << (double)(Clock::now() - last).count()/1000000000.0 << std:: endl << std::endl;
+      std::cout << "action: " << action.transpose() << std::endl;
+      last = Clock::now();
+    }
 
     robot_if->SendCommand();
 
     real_time_tools::Timer::sleep_sec(dt);
+    counter++;
   }
 
   return THREAD_FUNCTION_RETURN_VALUE;
