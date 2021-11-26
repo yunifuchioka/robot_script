71ea6a774271c7913d65c6e658dab3e097d6e9f7
yunifuchioka <fuchioka@cs.ubc.ca>
2021-11-26 13:44:45 -0800

    solo8 constructor

--- include/robot_script/solo8.hpp
+++ include/robot_script/solo8.hpp
@@ -23,10 +23,97 @@ class Solo8 {
   Solo8();
 
   /**
-   * initialize the robot by setting aligning the motors and calibrate the
-   * sensors to 0
+   * @brief initialize the robot by setting aligning the motors and calibrate
+   * the sensors to 0
    */
   void initialize(const std::string& network_id);
+
+  /**
+   * @brief send_target_torques sends the target currents to the motors
+   */
+  void send_target_joint_torque(const Eigen::Ref<Vector8d> target_joint_torque);
+
+  /**
+   * @brief acquire_sensors acquire all available sensors, WARNING !!!!
+   * this method has to be called prior to any getter to have up to date data.
+   */
+  void acquire_sensors();
+
+  /**
+   * @brief Asynchrounous calibrate the joints by moving to the next joint index
+   * position.
+   *
+   * @param home_offset_rad This is the angle between the index and the zero
+   * pose.
+   * @return true
+   * @return false
+   */
+  bool request_calibration(const Vector8d& home_offset_rad);
+
+  /**
+   * Sensor Data
+   */
+
+  /**
+   * @brief get_joint_positions
+   * @return  the joint angle of each module
+   * WARNING !!!!
+   * The method <acquire_sensors>"()" has to be called
+   * prior to any getter to have up to date data.
+   */
+  const Eigen::Ref<Vector8d> get_joint_positions() { return joint_positions_; }
+
+  /**
+   * @brief get_joint_velocities
+   * @return the joint velocities
+   * WARNING !!!!
+   * The method <acquire_sensors>"()" has to be called
+   * prior to any getter to have up to date data.
+   */
+  const Eigen::Ref<Vector8d> get_joint_velocities() {
+    return joint_velocities_;
+  }
+
+  /**
+   * @brief get_joint_torques
+   * @return the joint torques
+   * WARNING !!!!
+   * The method <acquire_sensors>"()" has to be called
+   * prior to any getter to have up to date data.
+   */
+  const Eigen::Ref<Vector8d> get_joint_torques() { return joint_torques_; }
+
+  /**
+   * @brief get_joint_torques
+   * @return the target joint torques
+   * WARNING !!!!
+   * The method <acquire_sensors>"()" has to be called
+   * prior to any getter to have up to date data.
+
+   */
+  const Eigen::Ref<Vector8d> get_joint_target_torques() {
+    return joint_target_torques_;
+  }
+
+  /**
+   * @brief get_joint_encoder_index
+   * @return the position of the index of the encoders a the motor level
+   * WARNING !!!!
+   * The method <acquire_sensors>"()" has to be called
+   * prior to any getter to have up to date data.
+   */
+  const Eigen::Ref<Vector8d> get_joint_encoder_index() {
+    return joint_encoder_index_;
+  }
+
+  /**
+   * @brief get_imu_attitude
+   * @return  the imu attitude
+   * WARNING !!!!
+   * The method <acquire_sensors>"()" has to be called
+   * prior to any getter to have up to date data.
+   */
+  const Eigen::Ref<Eigen::Vector3d> get_imu_attitude() { return imu_attitude_; }
 
  private:
   /**
@@ -38,14 +125,68 @@ class Solo8 {
   Vector8d motor_max_current_;      /**< Max appliable current before the robot
                                        shutdown. */
   Vector8d joint_zero_positions_;   /**< Offset to the theoretical "0" pose. */
-  Eigen::Array<double, 8, 1> max_joint_torques_; /**< Max joint torques (Nm) */
+  Vector8d max_joint_torques_;      /**< Max joint torques (Nm) */
 
+  /**
+   * Hardware status
+   */
+  /**
+   * @brief This gives the status (enabled/disabled) of each motors using the
+   * joint ordering convention.
+   */
+  std::array<bool, 8> motor_enabled_;
+  /**
+   * @brief This gives the status (enabled/disabled) of each motors using the
+   * joint ordering convention.
+   */
+  std::array<bool, 8> motor_ready_;
+  /**
+   * @brief This gives the status (enabled/disabled of the onboard control
+   * cards).
+   */
+  std::array<bool, 4> motor_board_enabled_;
+  /**
+   * @brief This gives the status (enabled/disabled of the onboard control
+   * cards).
+   */
+  std::array<int, 4> motor_board_errors_;
+
+  /**
+   * Joint data
+   */
+  Vector8d joint_positions_;
+  Vector8d joint_velocities_;
+  Vector8d joint_torques_;
+  Vector8d joint_target_torques_;
+  Vector8d joint_encoder_index_;
+
+  /**
+   * IMU data
+   */
+  Eigen::Vector3d imu_accelerometer_;
+  Eigen::Vector3d imu_gyroscope_;
+  Eigen::Vector3d imu_attitude_;
+  Eigen::Vector3d imu_linear_acceleration_;
+  Eigen::Vector4d imu_attitude_quaternion_;
+
+  /**
+   * Drivers communication objects
+   */
   /**
    * @brief Main board drivers.
    *
    * PC <- Ethernet/Wifi -> main board <- SPI -> Motor Board
    */
   std::shared_ptr<MasterBoardInterface> main_board_ptr_;
+
+  /** @brief If the physical estop is pressed or not. */
+  bool active_estop_;
+
+  /** @brief If the joint calibration is active or not. */
+  bool _is_calibrating;
+
+  /** @brief State of the solo robot. */
+  Solo8State state_;
 };
 
 }  // namespace solo
--- src/solo8.cpp
+++ src/solo8.cpp
@@ -7,12 +7,53 @@ namespace solo {
 using namespace odri_control_interface;
 
 Solo8::Solo8() {
+  /**
+   * Hardware properties
+   */
   motor_inertias_.setZero();
   motor_torque_constants_.setZero();
   joint_gear_ratios_.setZero();
   motor_max_current_.setZero();
   max_joint_torques_.setZero();
   joint_zero_positions_.setZero();
+  /**
+   * Hardware status
+   */
+  for (unsigned i = 0; i < motor_enabled_.size(); ++i) {
+    motor_enabled_[i] = false;
+    motor_ready_[i] = false;
+  }
+  for (unsigned i = 0; i < motor_board_enabled_.size(); ++i) {
+    motor_board_enabled_[0] = false;
+    motor_board_errors_[0] = 0;
+  }
+  /**
+   * Joint data
+   */
+  joint_positions_.setZero();
+  joint_velocities_.setZero();
+  joint_torques_.setZero();
+  joint_target_torques_.setZero();
+  joint_encoder_index_.setZero();
+  /**
+   * IMU data
+   */
+  imu_accelerometer_.setZero();
+  imu_gyroscope_.setZero();
+  imu_attitude_.setZero();
+  imu_linear_acceleration_.setZero();
+  imu_attitude_quaternion_.setZero();
+  /**
+   * Setup some known data
+   */
+  motor_max_current_.fill(400.0);  // TODO: set as paramters?
+  motor_torque_constants_.fill(0.025);
+  motor_inertias_.fill(0.045);
+  joint_gear_ratios_.fill(9.0);
+
+  active_estop_ = false;
+
+  state_ = Solo8State::initial;
 }
 
 void Solo8::initialize(const std::string& network_id) {
