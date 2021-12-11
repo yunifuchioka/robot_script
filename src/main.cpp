cce5df9112da15a274e6f27d1edbd1b52df147e6
yunifuchioka <fuchioka@cs.ubc.ca>
2021-12-10 16:24:02 -0800

    fix incorrect clipping direction in step in place motion

--- src/main.cpp
+++ src/main.cpp
@@ -44,8 +44,8 @@ static THREAD_FUNCTION_RETURN_TYPE control_loop(void* thread_data_void_ptr) {
     double safety_interp = std::min(1.0, std::max(t - safety_delay, 0.0));
 
     // get desired PD+torque targets from controller
-    controller.set_phase(8.0 * t);
-    controller.set_motion_type(PhaseController::MotionType::squat);
+    controller.set_phase(16.0 * t);
+    controller.set_motion_type(PhaseController::MotionType::step_in_place);
     controller.calc_control();
     joint_desired_positions = controller.get_desired_positions();
     joint_desired_velocities = controller.get_desired_velocities();
--- src/phase_controller.cpp
+++ src/phase_controller.cpp
@@ -42,16 +42,16 @@ void PhaseController::calc_control() {
     case MotionType::step_in_place: {
       desired_positions << M_PI / 4, -M_PI / 2, M_PI / 4, -M_PI / 2, -M_PI / 4,
           M_PI / 2, -M_PI / 4, M_PI / 2;
-      double amp = M_PI / 12;
+      double amp = M_PI / 8.0;
 
-      desired_positions(0) += std::min(amp * sin(phase_), 0.0);
-      desired_positions(1) += std::max(-2.0 * amp * sin(phase_), 0.0);
-      desired_positions(2) += std::min(amp * sin(phase_ + M_PI), 0.0);
-      desired_positions(3) += std::max(-2.0 * amp * sin(phase_ + M_PI), 0.0);
-      desired_positions(4) += std::max(amp * sin(phase_), 0.0);
-      desired_positions(5) += std::min(-2.0 * amp * sin(phase_), 0.0);
-      desired_positions(6) += std::max(amp * sin(phase_ + M_PI), 0.0);
-      desired_positions(7) += std::min(-2.0 * amp * sin(phase_ + M_PI), 0.0);
+      desired_positions(0) += std::max(amp * sin(phase_), 0.0);
+      desired_positions(1) += std::min(-2.0 * amp * sin(phase_), 0.0);
+      desired_positions(2) += std::max(amp * sin(phase_ + M_PI), 0.0);
+      desired_positions(3) += std::min(-2.0 * amp * sin(phase_ + M_PI), 0.0);
+      desired_positions(4) += std::min(amp * sin(phase_), 0.0);
+      desired_positions(5) += std::max(-2.0 * amp * sin(phase_), 0.0);
+      desired_positions(6) += std::min(amp * sin(phase_ + M_PI), 0.0);
+      desired_positions(7) += std::max(-2.0 * amp * sin(phase_ + M_PI), 0.0);
       break;
     }
   }
