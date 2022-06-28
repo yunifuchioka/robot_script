7d99f7e5a5fa297eabb4471296821fc8f0fa4296
yunifuchioka <fuchioka@cs.ubc.ca>
2022-06-28 14:52:45 -0700

    unsuccessful front hop motion

--- src/main.cpp
+++ src/main.cpp
@@ -36,11 +36,14 @@ static THREAD_FUNCTION_RETURN_TYPE control_loop(void* thread_data_void_ptr) {
   double t = 0.0;
 
   NetworkController controller(robot);
-  controller.initialize_network("06-23-trot-angvel01");
+  // controller.initialize_network("06-23-trot-angvel01");
+  // controller.initialize_network("06-25-no-ground-rand");
+  controller.initialize_network("06-26-ground-rand02");
   controller.set_motion_type(NetworkController::MotionType::traj);
 
   Eigen::MatrixXd ref_traj;
-  ref_traj = openData("../traj/06-22-trot.csv");
+  // ref_traj = openData("../traj/06-22-trot.csv");
+  ref_traj = openData("../traj/06-24-front-hop.csv");
   controller.set_traj(ref_traj);
 
   while (!CTRL_C_DETECTED) {
